ff5ce667b0a7e7d8ffadb4fc4faf0cf34f0dfea0
rohit <rohit.chandekar786@gmail.com>
2021-06-16 10:00:23 +0530

    setting up oauth20

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const app = express();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 const PORT = process.env.PORT || 8000;
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
-})
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: "/auth/google/callback"
+},(accessToken) =>{
+    console.log(accessToken);
+}));
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}))
+
+app.get('/auth/google/callback', passport.authenticate('google'))
 
 app.listen(PORT, ()=>{
     console.log('Running on PORT:', PORT);
